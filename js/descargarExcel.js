701540d1d602207cc904da1704eed9ea38a228df
J. Alfonso Mosco H <alfonsomozkoh@gmail.com>
2018-12-12 13:52:31 -0600

    Add files via upload

---
+++ js/descargarExcel.js
@@ -0,0 +1,16 @@
+		<!--Script para la creacion de Excel --> 
+		    function descargarExcel(){
+		        //Creamos un Elemento Temporal en forma de enlace
+		        var tmpElemento = document.createElement('a');
+		        // obtenemos la información desde el div que lo contiene en el html
+		        // Obtenemos la información de la tabla
+		        var data_type = 'data:application/vnd.ms-excel';
+		        var tabla_div = document.getElementById('tabla');
+		        var tabla_html = tabla_div.outerHTML.replace(/ /g, '%20');
+		        tmpElemento.href = data_type + ', ' + tabla_html;
+		        //Asignamos el nombre a nuestro EXCEL
+		        tmpElemento.download = 'ReporteAnalizadorCFDI.xlsx';
+		        // Simulamos el click al elemento creado para descargarlo
+		        tmpElemento.click();
+		    }
+		    descargarExcel();
